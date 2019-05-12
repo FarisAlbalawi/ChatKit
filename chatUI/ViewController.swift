1867096d630fe3a8013f22260e73ae4640ca8449
Faris Albalawi <46800827+FarisAlbalawi@users.noreply.github.com>
2019-05-12 04:32:25 -0400

    Initial Commit

---
+++ chatUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatUI
+//
+//  Created by Faris Albalawi on 5/12/19.
+//  Copyright © 2019 Faris Albalawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
